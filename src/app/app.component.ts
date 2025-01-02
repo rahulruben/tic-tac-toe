cd68eb6a596889aa14c7f124a1241801fdf78342
Rahulruben Mule <mulerahulruben@gmail.com>
2025-01-02 20:12:27 +0530

    Added implementation for tic tac toe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent implements OnInit {
+  board: string[] = [];
+  currentPlayer!: string;
+  winner!: string | null;
+
+  ngOnInit() {
+    this.resetBoard();
+  }
+
+  resetBoard() {
+    this.board = Array(9).fill('');
+    this.currentPlayer = 'X';
+    this.winner = null;
+  }
+
+  makeMove(index: number) {
+    if (this.board[index] || this.winner) return;
+    this.board[index] = this.currentPlayer;
+    if (this.checkWinner()) {
+      this.winner = this.currentPlayer;
+    } else {
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+    }
+  }
+
+  private checkWinner(): boolean {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 5],
+      [3, 5, 6],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const matched = winningCombinations.filter((combination: number[]) => {
+      const [a, b, c] = combination;
+
+      if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return !!matched.length;
+  }
 }
